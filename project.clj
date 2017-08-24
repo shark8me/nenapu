5d44d30dcb2627e5f5fafd200880f245d25a5c32
Kiran Karkera <kiran.karkera@gmail.com>
2017-08-24 13:40:44 +0800

    temp commit

--- project.clj
+++ project.clj
@@ -3,12 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [incanter "1.5.5"]
                  [iota "1.1.2"]
-                 [cc.artifice/clj-ml "0.3.5.1"]
+		[org.clojure/data.csv "0.1.3"]
+                 ;[cc.artifice/clj-ml "0.3.5.1"]
+                 [cc.artifice/clj-ml "0.6.0"]
                  [nz.ac.waikato.cms.weka/rotationForest "1.0.3" :exclusions [nz.ac.waikato.cms.weka/weka-dev]]
-                 [incanter-gorilla "0.1.1"]]
+                 ;[incanter-gorilla "0.1.1"]]
+                 [incanter-gorilla "0.1.0"]]
+  :jvm-opts ["-Xmx12g"] 
   :target-path "target/%s"
   :plugins [[lein-gorilla "0.3.4"]]
 )
